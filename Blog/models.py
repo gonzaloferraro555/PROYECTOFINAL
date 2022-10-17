6b233a2604caf0d486afabc7598feafc127bb913
Gonzalo Ferraro <gonzaferraro5@hotmail.com>
2022-10-17 17:18:09 -0300

    Modificando los models

--- Blog/models.py
+++ Blog/models.py
@@ -6,33 +6,36 @@ from django.db import models
 
 
 # Create your models here.
-class Curso(models.Model):
+class Cocinero(models.Model):
     nombre = models.CharField(max_length=40)
-    camada = models.IntegerField()
+    apellido = models.CharField(max_length=40)
+    edad = models.IntegerField()
+    email = models.EmailField()
+    Experiencia = models.CharField(max_length=4000)
     #Hay una herencia en estas clases, tomando la clase
     #de models importado de django.
     def __str__(self):
-        return f"Nombre:{self.nombre} - Apellido:{self.camada}"
+        return f"Nombre:{self.nombre} - Apellido:{self.apellid} - Edad: {self.edad} - Email: {self.email} - Experiencia: {self.Experiencia}"
    
-class Estudiante(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
+class Comentario(models.Model):
+    texto = models.CharField(max_length=500)
+    NombreYApellido = models.CharField(max_length=80)
     email = models.EmailField()
 
     def __str__(self):
-        return f"Nombre:{self.nombre} - Apellido:{self.apellido} - Email: {self.email}"
+        return f"Nombre:{self.NombreYApellido} - Email: {self.email} - Texto:{self.texto} "
     #Agregar este método en la clase, me permite definir una forma de mostrar para
     #ésta clase, un orden y formato de los atributos que la componen. Así, no veré un
     #objeto Estudiante desde la web, sino que veré la información contenida.
 
-class Profesor(models.Model):
+class Receta(models.Model):
     nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
+    texto = models.CharField(max_length=800)
     email = models.EmailField()
-    profesion = models.CharField(max_length=30)
+    autor = models.CharField(max_length=30)
 
     def __str__(self):
-        return f"Nombre:{self.nombre} - Apellido:{self.apellido} - Email: {self.email} - Profesión: {self.profesion}"
+        return f"Nombre:{self.nombre} - Texto:{self.texto} - Email: {self.email} - Autor: {self.autor}"
    
 
 class Entregable(models.Model):
